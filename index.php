009172b5318f71c1cb2dae00177de84897df561f
YUSMANEE <yusmanee.c@yru.ac.th>
2017-05-22 16:38:30 +0700

    สร้างฟอร์ม

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php 
 
-phpinfo();
+require_once("libs/Db.php"); 
+$objDb = new Db();
+$db = $objDb->database;
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PDO & CRUD</title>
+  </head>
+  <body>
+
+
+  </body>
+</html>
