e000d8a4d1361d4c30aaf8c5de2670845dd765ac
YUSMANEE <yusmanee.c@yru.ac.th>
2017-05-22 16:56:56 +0700

    สร้าง view แบบดึงหลายๆแถว

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php 
-
-require_once("libs/Db.php"); 
+<?php
+require_once("libs/Db.php");
 $objDb = new Db();
 $db = $objDb->database;
 ?>
@@ -13,6 +12,43 @@ $db = $objDb->database;
   </head>
   <body>
 
+    <?php
+    echo "Hello PDO";
+     ?>
+     <br/>
+     <a href="create.php">สร้าง Member</a>
+     <br/>
+
+     <table border='1'>
+       <thead>
+         <tr>
+           <th>#</th>
+           <th>ชื่อ-สกุล</th>
+           <th>สถานะ</th>
+           <th>Action</th>
+         </tr>
+       </thead>
+       <tbody>
+<?php
+$query = $db->prepare("SELECT * FROM member");
+$query->execute();//ประมวลผลคำสั่ง sql
+if($query->rowCount() > 0){ #rowCount เช็คจำนวนแถวที่ได้มา
+  while($row = $query->fetch(PDO::FETCH_ASSOC)){//ดึงข้อมูลแต่ละรอบใส่ใน $row
+ ?>
+         <tr>
+           <td><?=$row['id']?></td>
+           <td><?=$row['firstname'].' '.$row['lastname']?></td>
+           <td><?=$row['status']?></td>
+           <td>
+            <a href="#">ดู</a> | <a href="#">แก้ไข</a> | <a href="#">ลบ</a>
+           </td>
+         </tr>
+<?php
+  }#while
+}
+?>
+       </tbody>
+     </table>
 
   </body>
 </html>
